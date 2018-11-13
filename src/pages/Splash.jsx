8c32c90112b870882b4fd36b513f62a19c0292b2
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-11-13 18:51:46 -0500

    working on splash page

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -3,8 +3,16 @@ import React, { Component } from 'react'
 class Splash extends Component {
   render() {
     return (
-      <div>
-        <h2>this is the splash page</h2>
+      <div className="splash-body">
+        <main className="splash-main">
+          <h1 className="splash-header">
+            Welcome to the sweetest app in the world!
+          </h1>
+          <p className="splash-welcome">Have a blast here, friend!</p>
+          <div>
+            <button className="login">Log In</button>
+          </div>
+        </main>
       </div>
     )
   }
