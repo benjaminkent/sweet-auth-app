45da2f736d294b0e51eaca0c7746419e851a41f3
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-11-13 18:35:24 -0500

    splash and home pages created

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import './App.css'
+import Home from './pages/Home'
+import Splash from './pages/Splash'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Splash} />
+            <Route exact path="/home" component={Home} />
+          </Switch>
+        </Router>
+      </>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/pages/Splash.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Splash extends Component {
+  render() {
+    return (
+      <div>
+        <h2>this is the splash page</h2>
+      </div>
+    )
+  }
+}
+
+export default Splash
