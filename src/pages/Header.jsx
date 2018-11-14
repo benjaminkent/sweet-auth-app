0b54b69cd1d1fe411ef81918403be78dc8c743f2
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-11-14 14:37:33 -0500

    working on buttons

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
+import Auth from '../auth/auth.js'
+
+const auth = new Auth()
 
 class Header extends Component {
+  _logout = () => {
+    auth.logout()
+  }
+
   render() {
     return (
       <div>
         <header>
           <h2>Hello !</h2>
-          <button className="logout">Log out</button>
+          <button onClick={this._logout} className="logout">
+            Log out
+          </button>
         </header>
       </div>
     )
